cb3a01a4fd073fdc343a5819f81424aac0abfc5a
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 22:59:28 -0500

    upload goto.c

--- Lab1_UnixTerminal/part2/goto.c
+++ Lab1_UnixTerminal/part2/goto.c
@@ -1,1 +1,14 @@
 // Write a C program using the goto command and a label.
+
+
+#include<stdio.h>
+int main() {
+	int n = 0;
+   	printf("input a string: ");
+        	loop: if (getchar()!='\n') {
+        	n++;
+        	goto loop;
+   	 }	
+  	printf("output: %d\n",n);
+	return 0;
+}
