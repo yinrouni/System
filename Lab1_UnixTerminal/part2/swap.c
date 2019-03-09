93bedd95c3b2d1fddc9e5f8fc6921a4955e8359c
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 23:00:18 -0500

    upload swap.c

--- Lab1_UnixTerminal/part2/swap.c
+++ Lab1_UnixTerminal/part2/swap.c
@@ -1,1 +1,19 @@
 // Write a C program that swaps two integers in a function
+#include <stdio.h>
+
+void swap(int* a, int* b){
+	int aa = *a;
+	int bb = *b;
+	*a = bb;
+	*b = aa;
+}
+
+int main (){
+	int a = 1;
+	int b = -1;
+	printf("a: %d, b: %d\n", a, b);
+	printf("===== swap =====\n");
+	swap(&a, &b);
+	printf("a: %d, b: %d\n",a, b);
+	return 0;
+}
