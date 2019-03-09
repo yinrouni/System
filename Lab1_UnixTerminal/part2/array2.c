f756ee86ed7d69e84d65be1b987cbf56e94d1306
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 22:58:45 -0500

    upload array2.c

--- Lab1_UnixTerminal/part2/array2.c
+++ Lab1_UnixTerminal/part2/array2.c
@@ -1,1 +1,14 @@
 // Write a C program called array2.c that has an array of 400 integers in the function of main that is dynamically allocated.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int* a = (int*) malloc(sizeof (int) * 400);
+	a[0] = 70;
+	a[100] = 100;
+	a[400] = 444;
+	free(a);
+	return 0;
+}
+
