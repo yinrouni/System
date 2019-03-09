cf3cce8ddcdda48e80a87b7f3c4770c7cf796c82
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 23:01:50 -0500

    upload switch.c

--- Lab1_UnixTerminal/part2/switch.c
+++ Lab1_UnixTerminal/part2/switch.c
@@ -1,1 +1,31 @@
 // Write a C program using a switch statement
+
+#include <stdio.h>
+
+int main () {
+    int grade = 90; 
+    switch(grade) {
+        case 100 :
+            printf("Excellent!\n" );
+            break;
+        case 90 :
+            printf("Well done\n" ); 
+            break;
+        case 80 :
+            printf("Well done\n" );
+            break;
+        case 60 :
+            printf("You passed\n" );
+            break;
+        case 40 :
+            printf("Better try again\n" );
+            break;
+        default :
+            printf("Invalid grade\n" );
+    }
+    
+    printf("Your grade is  %d\n", grade );
+    
+    return 0;
+}
+
