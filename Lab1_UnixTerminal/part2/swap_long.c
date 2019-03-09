90436fa6ecc5ac54349103985aaee64b5e46e523
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 23:01:26 -0500

    upload swap_long.c

--- Lab1_UnixTerminal/part2/swap_long.c
+++ Lab1_UnixTerminal/part2/swap_long.c
@@ -1,1 +1,18 @@
 // Now modify your program to swap two long's.
+# include <stdio.h>
+
+int main(){
+	long a = 12345678;
+	long b = -12345678;
+	printf("a: %ld\n", a);
+	printf("b: %ld\n", b);
+	long temp;
+	temp = a;
+	a = b;
+ 	b = temp;
+	printf("===== swap =====\n");
+	printf("a: %ld\n", a);
+	printf("b: %ld\n", b);
+	return 0;
+}
+
