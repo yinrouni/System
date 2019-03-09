c7634ec1f00a357232f68e126129910aa05d86e1
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 23:00:48 -0500

    upload swap_int.c

--- Lab1_UnixTerminal/part2/swap_int.c
+++ Lab1_UnixTerminal/part2/swap_int.c
@@ -1,1 +1,18 @@
 // Write a C program that swaps two integers(in the main body of code).
+
+#include <stdio.h>
+
+int main(){
+	int a = 1;
+	int b = -1;
+	printf("a: %d\n", a);
+	printf("b: %d\n", b);
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	printf("====== swap ======\n");
+	printf("a: %d\n", a);
+	printf("b: %d\n", b);
+	return 0;
+}
