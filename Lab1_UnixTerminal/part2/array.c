8bd46669c0e346435e858d9ddd32610ba1492213
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 22:56:09 -0500

    upload array.c

--- Lab1_UnixTerminal/part2/array.c
+++ Lab1_UnixTerminal/part2/array.c
@@ -1,1 +1,11 @@
 // Write a C program called array.c that has an array of 400 integers in the function of main.
+
+
+int main(){
+	int a[400];
+	a[0] = 72;
+	a[70] = 56;
+	a[100] = 100;
+	a[400] = 444;
+	return 0;
+}
