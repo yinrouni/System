fcea74513d184e8b947cf9d6e2510a629c629873
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-08 22:52:40 -0500

    upload add.c

--- Lab1_UnixTerminal/part2/add.c
+++ Lab1_UnixTerminal/part2/add.c
@@ -1,1 +1,13 @@
 // Write a C program that calls an add function(long add(long a, long b).
+
+#include <stdio.h>
+long add (long a, long b){
+	long result = a + b;
+	return result;
+}
+int main(){
+	long a = 123;
+	long b = 321;
+	printf("%ld + %ld = %ld\n", a, b, add(a, b));
+	return 0;
+}
