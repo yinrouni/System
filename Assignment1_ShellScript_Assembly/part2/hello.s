336daa7b479d06bab591bc894e5f4556992eaf3c
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-13 15:43:20 -0400

    upload hello.s

--- Assignment1_ShellScript_Assembly/part2/hello.s
+++ Assignment1_ShellScript_Assembly/part2/hello.s
@@ -1,1 +1,20 @@
 # Your code here
+.global _start
+
+  .text
+
+  _start:
+  	# Write a message to the screen
+  	mov $1, %rax
+  	mov $1, %rdi
+  	mov $message, %rsi
+  	mov $13, %rdx
+  	syscall
+
+  	# Exit the program
+  	mov $60, %rax
+  	xor %rdi, %rdi
+  	syscall
+
+  message:
+  	.ascii "Hello, World\n"
