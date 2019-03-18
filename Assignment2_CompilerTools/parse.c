8de961d653ea51e3edb90cd1d873eb7046eee77c
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-17 22:29:12 -0400

    upload parse

--- Assignment2_CompilerTools/parse.c
+++ Assignment2_CompilerTools/parse.c
@@ -1,1 +1,20 @@
 // Implement a working parser in this file that splits text into individual tokens.
+
+#include <stdio.h>
+#include <string.h>
+int main(int argc, const char * argv[]) {
+    char str[strlen(argv[1])];
+    int i;
+    for (i = 0; i < strlen(argv[1]); i ++){
+        str[i] = argv[1][i];
+        
+    }
+    char* pch;
+    pch = strtok(str, " ");
+    while(pch != NULL){
+        printf("%s\n", pch);
+        pch = strtok(NULL, " ");
+    }
+    return 0;
+}
+
