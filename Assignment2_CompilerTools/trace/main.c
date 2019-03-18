9a9e437c7f00766233c66e189cc6e2ef5a2dfb1e
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-17 22:32:21 -0400

    add timer

--- Assignment2_CompilerTools/trace/main.c
+++ Assignment2_CompilerTools/trace/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <time.h>
 
 void msg(){
 	printf("Hello!\n");
@@ -7,11 +7,13 @@ void msg(){
 
 
 int main(){
-
+	time_t start, end;
+	start = time(NULL);
 	int i;
 	for(i= 0;i  < 10; ++i){
 		msg();
 	}
-
+	end = time(NULL);
+	printf("time: %f\n",difftime(end, start)); 
 	return 0;
 }
