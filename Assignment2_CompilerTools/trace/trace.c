1acf1ef2f58e3199951ac10893f7390acd2e873f
Rouni Yin <yinrouni@login-students.ccs.neu.edu>
2019-03-21 22:35:04 -0400

    add counter

--- Assignment2_CompilerTools/trace/trace.c
+++ Assignment2_CompilerTools/trace/trace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+int counter = 0;
 __attribute__((no_instrument_function))
 void __cyg_profile_func_enter(void *this_fn, void *call_site){
-
+	counter ++;
 	printf("Function entered %p from %p\n", this_fn,call_site);
 }
 
